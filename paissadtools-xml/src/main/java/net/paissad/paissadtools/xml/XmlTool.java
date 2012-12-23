23a3d4da1382a01718b3d3745cf171f4b032212d
paissad <paissad@gmail.com>
2012-12-24 00:33:47 +0100

    Removed boilerplate code from XML tool module.

--- paissadtools-xml/src/main/java/net/paissad/paissadtools/xml/XmlTool.java
+++ paissadtools-xml/src/main/java/net/paissad/paissadtools/xml/XmlTool.java
@@ -1,24 +1,6 @@
 package net.paissad.paissadtools.xml;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-import javax.xml.XMLConstants;
-import javax.xml.transform.sax.SAXSource;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
-
 import net.paissad.paissadtools.api.ITool;
-import net.paissad.paissadtools.xml.exception.XmlToolException;
-
-import org.xml.sax.ErrorHandler;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
 
 /**
  * XML tool.
@@ -26,65 +8,5 @@ import org.xml.sax.SAXParseException;
  * @author paissad
  */
 public class XmlTool implements ITool {
-
-    public static void validate(final File xmlFile, final File xsdFile) throws XmlToolException {
-
-        BufferedInputStream xsdStream = null;
-        try {
-            xsdStream = new BufferedInputStream(new FileInputStream(xsdFile));
-            final SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            final InputSource xsdSource = new InputSource(xsdStream);
-            final SAXSource saxSource = new SAXSource(xsdSource);
-            final Schema schema = schemaFactory.newSchema(saxSource);
-            final Validator validator = schema.newValidator();
-            validator.setErrorHandler(new CustomErrorHandler());
-            validator.validate(new StreamSource(xmlFile));
-
-        } catch (Exception e) {
-            final String errMsg = "Error while validating the XML file -> " + xmlFile.getAbsolutePath() + " : "
-                    + e.getMessage();
-            throw new XmlToolException(errMsg, e);
-
-        } finally {
-            if (xsdStream != null) {
-                try {
-                    xsdStream.close();
-                } catch (IOException ioe) {
-                    ioe.printStackTrace(System.err);
-                }
-            }
-        }
-    }
-
-    private static class CustomErrorHandler implements ErrorHandler {
-
-        public CustomErrorHandler() {
-        }
-
-        @Override
-        public void warning(SAXParseException e) throws SAXException {
-            this.printInfoAndThrowException("WARNING", e);
-        }
-
-        @Override
-        public void error(SAXParseException e) throws SAXException {
-            this.printInfoAndThrowException("ERROR", e);
-        }
-
-        @Override
-        public void fatalError(SAXParseException e) throws SAXException {
-            this.printInfoAndThrowException("FATAL", e);
-        }
-
-        private void printInfoAndThrowException(final String errorLevel, final SAXParseException e) throws SAXException {
-            System.err.println("[" + errorLevel + "]");
-            System.err.println("   Public ID: " + e.getPublicId());
-            System.err.println("   System ID: " + e.getSystemId());
-            System.err.println("   Line number: " + e.getLineNumber());
-            System.err.println("   Column number: " + e.getColumnNumber());
-            System.err.println("   Message: " + e.getMessage());
-            throw new SAXException(e);
-        }
-    }
 
 }
